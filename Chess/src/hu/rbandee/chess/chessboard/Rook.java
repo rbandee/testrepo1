74320750d363a73e9fa25238aa97a7ecf7e8e346
rbandee <rbandee@gmail.com>
2014-09-24 21:36:54 +0200

    [WIP] rook movecheck skeleton

--- Chess/src/hu/rbandee/chess/chessboard/Rook.java
+++ Chess/src/hu/rbandee/chess/chessboard/Rook.java
@@ -8,6 +8,42 @@ public class Rook extends Piece {
 
 	@Override
 	protected boolean isMoveValid(Square newPosition) {
+		boolean valid;
+		if (newPosition.isFree()){
+			if (validVerticalStep(newPosition) || validHorizontalStep(newPosition)){
+				valid = true;
+			} else{
+				valid = false;
+			}
+		}else if (isOpponent(newPosition)) {
+			if (capture(newPosition)) {
+				valid = true;
+			} else {
+				valid = false;
+			}
+
+		} else {
+			valid = false;
+		}
+		return valid;
+	}
+
+	private boolean capture(Square newPosition) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	private boolean isOpponent(Square newPosition) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	private boolean validHorizontalStep(Square newPosition) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	private boolean validVerticalStep(Square newPosition) {
 		// TODO Auto-generated method stub
 		return false;
 	}
