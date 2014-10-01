25e2b99f7d3bd97fb548a060256a159aa107d035
eandrks <andras.rokas@ericsson.com>
2014-10-01 13:37:53 +0200

    Pawn tests refactored

--- Chess/test/hu/rbandee/chess/pieces/PawnTest.java
+++ Chess/test/hu/rbandee/chess/pieces/PawnTest.java
@@ -5,7 +5,6 @@ import static org.testng.AssertJUnit.assertTrue;
 import hu.rbandee.chess.chessboard.ChessBoard;
 import hu.rbandee.chess.chessboard.PieceType;
 import hu.rbandee.chess.chessboard.Side;
-import hu.rbandee.chess.pieces.Pawn;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeTest;
@@ -13,10 +12,16 @@ import org.testng.annotations.Test;
 
 public class PawnTest {
 	private ChessBoard testBoard;
+	private Pawn whitePawn;
+	private Pawn whitePawn2;
+	private Pawn blackPawn;
 
 	@BeforeTest
 	public void initBoard() {
 		testBoard = new ChessBoard();
+		whitePawn = (Pawn) testBoard.createNewPiece(ChessBoard.OUTOFBOARD, Side.White, PieceType.Pawn);
+		whitePawn2 = (Pawn) testBoard.createNewPiece(ChessBoard.OUTOFBOARD, Side.White, PieceType.Pawn);
+		blackPawn = (Pawn) testBoard.createNewPiece(ChessBoard.OUTOFBOARD, Side.Black, PieceType.Pawn);
 	}
 
 	@AfterMethod
@@ -26,90 +31,89 @@ public class PawnTest {
 
 	@Test
 	public void neg_stay_in_same_place_is_not_a_move() {
-		final Pawn apawn = (Pawn) testBoard.createNewPiece("A2", Side.White, PieceType.Pawn);
-		assertFalse(apawn.isMoveValid(testBoard.getSquare("A2")));
+		whitePawn.setPosition("A2");
+		assertFalse(whitePawn.isMoveValid(testBoard.getSquare("A2")));
 	}
 
 	@Test
 	public void one_step_forward_from_start_is_valid_move() {
-		final Pawn apawn = (Pawn) testBoard.createNewPiece("A2", Side.White, PieceType.Pawn);
-		assertTrue(apawn.isMoveValid(testBoard.getSquare("A3")));
+		whitePawn.setPosition("A2");
+		assertTrue(whitePawn.isMoveValid(testBoard.getSquare("A3")));
 	}
 
 	@Test
 	public void one_step_forward_is_valid_move() {
-		final Pawn apawn = (Pawn) testBoard.createNewPiece("C4", Side.White, PieceType.Pawn);
-		assertTrue(apawn.isMoveValid(testBoard.getSquare("C5")));
+		whitePawn.setPosition("C4");
+		assertTrue(whitePawn.isMoveValid(testBoard.getSquare("C5")));
 	}
 
 	@Test
 	public void one_step_forward_with_black_is_valid_move() {
-		final Pawn apawn = (Pawn) testBoard.createNewPiece("B7", Side.Black, PieceType.Pawn);
-		assertTrue(apawn.isMoveValid(testBoard.getSquare("B6")));
+		blackPawn.setPosition("B7");
+		assertTrue(blackPawn.isMoveValid(testBoard.getSquare("B6")));
 	}
 
 	@Test
 	public void two_steps_forward_is_valid_move_as_opening() {
-		final Pawn apawn = (Pawn) testBoard.createNewPiece("A2", Side.White, PieceType.Pawn);
-		assertTrue(apawn.isMoveValid(testBoard.getSquare("A4")));
+		whitePawn.setPosition("A2");
+		assertTrue(whitePawn.isMoveValid(testBoard.getSquare("A4")));
 	}
 
 	@Test
 	public void neg_two_steps_forward_not_from_starting_position() {
-		final Pawn apawn = (Pawn) testBoard.createNewPiece("A3", Side.White, PieceType.Pawn);
-		assertFalse(apawn.isMoveValid(testBoard.getSquare("A5")));
+		whitePawn.setPosition("A3");
+		assertFalse(whitePawn.isMoveValid(testBoard.getSquare("A5")));
 	}
 
 	@Test
 	public void neg_could_not_step_to_a_square_too_far() {
-		final Pawn apawn = (Pawn) testBoard.createNewPiece("A3", Side.White, PieceType.Pawn);
-		assertFalse(apawn.isMoveValid(testBoard.getSquare("E6")));
+		whitePawn.setPosition("A3");
+		assertFalse(whitePawn.isMoveValid(testBoard.getSquare("E6")));
 	}
 
 	@Test
 	public void capture_opponent_piece() {
-		final Pawn apawn = (Pawn) testBoard.createNewPiece("E5", Side.White, PieceType.Pawn);
-		final Pawn opponentPawn = (Pawn) testBoard.createNewPiece("D6", Side.Black, PieceType.Pawn);
-		assertTrue(apawn.isMoveValid(testBoard.getSquare("D6")));
+		whitePawn.setPosition("E5");
+		blackPawn.setPosition("D6");
+		assertTrue(whitePawn.isMoveValid(testBoard.getSquare("D6")));
 	}
 
 	@Test
 	public void neg_capture_opponent_piece() {
-		final Pawn apawn = (Pawn) testBoard.createNewPiece("E5", Side.White, PieceType.Pawn);
-		final Pawn opponentPawn = (Pawn) testBoard.createNewPiece("D7", Side.Black, PieceType.Pawn);
-		assertFalse(apawn.isMoveValid(testBoard.getSquare("D6")));
+		whitePawn.setPosition("E5");
+		blackPawn.setPosition("D7");
+		assertFalse(whitePawn.isMoveValid(testBoard.getSquare("D6")));
 	}
 
 	@Test
 	public void capture_opponent_piece_with_black() {
-		final Pawn apawn = (Pawn) testBoard.createNewPiece("D6", Side.Black, PieceType.Pawn);
-		final Pawn opponentPawn = (Pawn) testBoard.createNewPiece("E5", Side.White, PieceType.Pawn);
-		assertTrue(apawn.isMoveValid(testBoard.getSquare("E5")));
+		blackPawn.setPosition("D6");
+		whitePawn.setPosition("E5");
+		assertTrue(blackPawn.isMoveValid(testBoard.getSquare("E5")));
 	}
 
 	@Test
 	public void neg_capture_own_piece() {
-		final Pawn apawn = (Pawn) testBoard.createNewPiece("E5", Side.White, PieceType.Pawn);
-		final Pawn opponentPawn = (Pawn) testBoard.createNewPiece("F6", Side.White, PieceType.Pawn);
-		assertFalse(apawn.isMoveValid(testBoard.getSquare("F6")));
+		whitePawn.setPosition("E5");
+		whitePawn2.setPosition("F6");
+		assertFalse(whitePawn.isMoveValid(testBoard.getSquare("F6")));
 	}
 
 	@Test
 	public void capture_opponent_en_passant() {
-		final Pawn apawn = (Pawn) testBoard.createNewPiece("E2", Side.White, PieceType.Pawn);
-		final Pawn opponentPawn = (Pawn) testBoard.createNewPiece("D4", Side.Black, PieceType.Pawn);
-
-		apawn.move(testBoard.getSquare("E4"));
-		assertTrue(opponentPawn.isMoveValid(testBoard.getSquare("E3")));
+		whitePawn.setPosition("E2");
+		blackPawn.setPosition("D4");
+		whitePawn.move(testBoard.getSquare("E4"));
+		assertTrue(blackPawn.isMoveValid(testBoard.getSquare("E3")));
 	}
 
 	@Test
 	public void neg_capture_opponent_en_passant_not_immediately() {
-		final Pawn apawn = (Pawn) testBoard.createNewPiece("E2", Side.White, PieceType.Pawn);
-		final Pawn otherPawn = (Pawn) testBoard.createNewPiece("D2", Side.White, PieceType.Pawn);
-		final Pawn opponentPawn = (Pawn) testBoard.createNewPiece("D4", Side.Black, PieceType.Pawn);
-		apawn.move(testBoard.getSquare("E4"));
-		otherPawn.move(testBoard.getSquare("D3"));
-		assertFalse(opponentPawn.isMoveValid(testBoard.getSquare("E3")));
+		whitePawn.setPosition("E2");
+		whitePawn2.setPosition("D2");
+		blackPawn.setPosition("D4");
+		whitePawn.move(testBoard.getSquare("E4"));
+		whitePawn2.move(testBoard.getSquare("D3"));
+		assertFalse(blackPawn.isMoveValid(testBoard.getSquare("E3")));
 	}
 }
