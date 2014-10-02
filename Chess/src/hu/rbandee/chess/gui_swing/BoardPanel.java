11384b6b9e37fafe1a8e307a40842305e8922366
rbandee <rbandee@gmail.com>
2014-10-02 20:22:26 +0200

    New: All piece signs are shown on the gui

--- Chess/src/hu/rbandee/chess/gui_swing/BoardPanel.java
+++ Chess/src/hu/rbandee/chess/gui_swing/BoardPanel.java
@@ -1,11 +1,18 @@
 package hu.rbandee.chess.gui_swing;
 
 import hu.rbandee.chess.chessboard.ChessBoard;
+import hu.rbandee.chess.chessboard.Side;
 import hu.rbandee.chess.other.Application;
+import hu.rbandee.chess.pieces.Bishop;
+import hu.rbandee.chess.pieces.King;
+import hu.rbandee.chess.pieces.Knight;
 import hu.rbandee.chess.pieces.Pawn;
 import hu.rbandee.chess.pieces.Piece;
+import hu.rbandee.chess.pieces.Queen;
+import hu.rbandee.chess.pieces.Rook;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Rectangle2D;
@@ -15,8 +22,10 @@ import javax.swing.JComponent;
 public class BoardPanel extends JComponent {
 	private final int squaresInRow = 8;
 	private final int squaresInColumn = 8;
-	private final Color darkColor = Color.DARK_GRAY;
-	private final Color lightColor = Color.WHITE;
+	private final Color darkSquareColor = Color.LIGHT_GRAY;
+	private final Color lightSquareColor = Color.WHITE;
+	private final Color darkPieceColor = Color.DARK_GRAY;
+	private final Color lightPieceColor = Color.ORANGE;
 	private int squareHeight;
 	private int squareWidth;
 	private ChessBoard board;
@@ -45,12 +54,12 @@ public class BoardPanel extends JComponent {
 	}
 
 	private void drawLightSquare(final Graphics2D g2d, int col, int row) {
-		g2d.setColor(lightColor);
+		g2d.setColor(lightSquareColor);
 		g2d.fill(new Rectangle2D.Double(col * squareWidth, row * squareHeight, squareWidth, squareHeight));
 	}
 
 	private void drawDarkSquare(final Graphics2D g2d, int col, int row) {
-		g2d.setColor(darkColor);
+		g2d.setColor(darkSquareColor);
 		g2d.fill(new Rectangle2D.Double(col * squareWidth, row * squareHeight, squareWidth, squareHeight));
 	}
 
@@ -67,16 +76,30 @@ public class BoardPanel extends JComponent {
 	}
 
 	private void drawPiece(Graphics2D g2d, Piece piece) {
-		if (piece instanceof Pawn) {
-			drawPawn(g2d, (Pawn) piece);
-		}
-	}
-
-	private void drawPawn(Graphics2D g2d, Pawn piece) {
 		int x = piece.getPosition().getColumn() * squareWidth + squareWidth / 2;
-		int y = piece.getPosition().getRow() * squareHeight + squareHeight / 2;
-		g2d.setColor(Color.green);
-		g2d.drawString("P", x, y);
+		int y = (squaresInColumn - piece.getPosition().getRow() - 1) * squareHeight + squareHeight / 2;
+		if (piece.getSide().equals(Side.White)){
+			g2d.setColor(lightPieceColor);
+		} else{
+			g2d.setColor(darkPieceColor);
+		}
+
+		g2d.setFont(new Font("TimesRoman", Font.BOLD, 32));
+		if (piece instanceof Pawn) {
+			g2d.drawString("P", x, y);
+		} else if (piece instanceof King) {
+			g2d.drawString("K", x, y);
+		} else if (piece instanceof King) {
+			g2d.drawString("K", x, y);
+		} else if (piece instanceof Knight) {
+			g2d.drawString("N", x, y);
+		} else if (piece instanceof Queen) {
+			g2d.drawString("Q", x, y);
+		} else if (piece instanceof Bishop) {
+			g2d.drawString("B", x, y);
+		} else if (piece instanceof Rook) {
+			g2d.drawString("R", x, y);
+		}
 	}
 
 	private ChessBoard getBoard() {
